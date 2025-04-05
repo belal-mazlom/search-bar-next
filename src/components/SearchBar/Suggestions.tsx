38b3faa487c9a9c3281f3c0a41a8e2ed9c78f17e
Belal Al Mazlom <b.mazlom@gmail.com>
2025-04-06 01:15:51 +1030

    Add Suggestions component

--- src/components/SearchBar/Suggestions.tsx
+++ src/components/SearchBar/Suggestions.tsx
@@ -1,21 +1,21 @@
 import Image from "next/image";
 import type { Suggestions } from "@/types/SearchBar";
-
+import { formatPrice } from "@/utils";
 const Suggestions = ({ items }: Suggestions) => {
   return (
-    <div className="absolute top-4 left-0 w-full bg-white shadow-md grid grid-cols-2 md:grid-cols-4 gap-4">
+    <div className="absolute top-[54px] left-0 w-full px-4 py-2 bg-background shadow-md grid grid-cols-1 md:grid-cols-2 gap-4">
       {items.map((item) => (
-        <div key={item.id} className="grid grid-cols-[1fr_auto] gap-4">
+        <div key={item.id} className="grid grid-cols-[100px_auto] gap-4">
           <Image
             src={item.image}
             alt={item.name}
             width={100}
             height={100}
-            className="w-10 h-10 rounded-md"
+            className="rounded-md"
           />
-          <div className="flex flex-col gap-2">
-            <h3 className="text-sm font-medium">{item.name}</h3>
-            <p className="text-sm text-gray-500">{item.price}</p>
+          <div className="flex flex-col gap-2 justify-center">
+            <h3 className="text-lg font-bold uppercase">{item.name}</h3>
+            <p className="text-md text-gray-500">{formatPrice(item.price)}</p>
           </div>
         </div>
       ))}
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,6 +1,33 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import Suggestions from "./Suggestions";
 
 const SearchBar = () => {
+  const items = [
+    {
+      id: "1",
+      name: "Product 1",
+      price: 100,
+      image: "https://cdn.dummyjson.com/products/images/sports-accessories/Tennis%20Ball/thumbnail.png",
+    },
+    {
+      id: "2",
+      name: "Product 2",
+      price: 200,
+      image: "https://cdn.dummyjson.com/products/images/sports-accessories/Tennis%20Ball/thumbnail.png",
+    },
+    {
+      id: "3",
+      name: "Product 3",
+      price: 300,
+      image: "https://cdn.dummyjson.com/products/images/sports-accessories/Tennis%20Ball/thumbnail.png",
+    },
+    {
+      id: "4",
+      name: "Product 4",
+      price: 400,
+      image: "https://cdn.dummyjson.com/products/images/sports-accessories/Tennis%20Ball/thumbnail.png",
+    },
+  ];
   return (
     <div className="relative py-2 px-4">
       <MagnifyingGlassIcon className="absolute left-8 top-5 h-5 w-5 text-gray-400" />
@@ -9,6 +36,7 @@ const SearchBar = () => {
         placeholder="Search"
         className="w-full rounded-full border-2 border-gray-300 p-2 pl-10"
       />
+      <Suggestions items={items} />
     </div>
   );
 };
