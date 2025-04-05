00dbd91b81b075199c050697fcc991b564ed696d
Belal Al Mazlom <b.mazlom@gmail.com>
2025-04-06 01:15:26 +1030

    Add formatPrice

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
+
+export function formatPrice(price: number) {
+  return price.toLocaleString("en-AU", {
+    style: "currency",
+    currency: "AUD",
+    minimumFractionDigits: 2,
+  });
+}
