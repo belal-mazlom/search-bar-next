97cfc0ec13f50d6a56a6bd138abf5327426e9fb1
Belal Al Mazlom <b.mazlom@gmail.com>
2025-04-06 02:29:17 +1030

    Fix responsive for search suggestions

--- src/components/SearchBar/Suggestions.tsx
+++ src/components/SearchBar/Suggestions.tsx
@@ -3,22 +3,24 @@ import type { Suggestions } from "@/types/SearchBar";
 import { formatPrice } from "@/utils";
 const Suggestions = ({ items }: Suggestions) => {
   return (
-    <div className="absolute top-[54px] left-0 w-full px-4 py-2 bg-background shadow-md grid grid-cols-1 md:grid-cols-2 gap-4">
-      {items.map((item) => (
-        <div key={item.id} className="grid grid-cols-[100px_auto] gap-4">
-          <Image
-            src={item.thumbnail}
-            alt={item.title}
-            width={100}
-            height={100}
-            className="rounded-md"
-          />
-          <div className="flex flex-col gap-2 justify-center">
-            <h3 className="text-lg font-bold uppercase">{item.title}</h3>
-            <p className="text-md text-gray-500">{formatPrice(item.price)}</p>
+    <div className="absolute top-[54px] md:top-[98px] left-0 w-full px-4 py-2 bg-background shadow-md">
+      <div className="mx-auto max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {items.map((item) => (
+          <div key={item.id} className="grid grid-cols-[100px_auto] gap-4">
+            <Image
+              src={item.thumbnail}
+              alt={item.title}
+              width={100}
+              height={100}
+              className="rounded-md"
+            />
+            <div className="flex flex-col gap-2 justify-center">
+              <h3 className="text-lg font-bold uppercase">{item.title}</h3>
+              <p className="text-md text-gray-500">{formatPrice(item.price)}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -35,26 +35,30 @@ const SearchBar = () => {
     };
     fetchItems();
 
-    return () => { debouncedSearch(''); };
+    return () => {
+      debouncedSearch("");
+    };
   }, [search, debouncedSearch]);
 
   return (
-    <div className="relative py-2 px-4">
-      <MagnifyingGlassIcon className="absolute left-8 top-5 h-5 w-5 text-gray-400" />
-      <input
-        type="text"
-        placeholder="Search"
-        className="w-full rounded-full border-2 border-gray-300 p-2 pl-10"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      {search.length > 0 && (
-        <XMarkIcon
-          className="absolute right-8 top-5 h-5 w-5 text-gray-800"
-          onClick={() => setSearch("")}
+    <div className="relative md:static">
+      <div className="relative py-2 px-4">
+        <MagnifyingGlassIcon className="absolute left-8 top-5 h-5 w-5 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full rounded-full border-2 border-gray-300 p-2 pl-10"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
-      )}
-      <Suggestions items={items} />
+        {search.length > 0 && (
+          <XMarkIcon
+            className="absolute right-8 top-5 h-5 w-5 text-gray-800"
+            onClick={() => setSearch("")}
+          />
+        )}
+      </div>
+      {items.length > 0 && <Suggestions items={items} />}
     </div>
   );
 };
