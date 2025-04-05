26009e4de8adbbd2fd3dd99a4345451dc0bec5be
Belal Al Mazlom <b.mazlom@gmail.com>
2025-04-06 01:19:53 +1030

    Add clear button to search input

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,7 +1,11 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+"use client";
+
+import { useState } from "react";
+import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Suggestions from "./Suggestions";
 
 const SearchBar = () => {
+  const [search, setSearch] = useState("");
   const items = [
     {
       id: "1",
@@ -28,6 +32,7 @@ const SearchBar = () => {
       image: "https://cdn.dummyjson.com/products/images/sports-accessories/Tennis%20Ball/thumbnail.png",
     },
   ];
+
   return (
     <div className="relative py-2 px-4">
       <MagnifyingGlassIcon className="absolute left-8 top-5 h-5 w-5 text-gray-400" />
@@ -35,7 +40,15 @@ const SearchBar = () => {
         type="text"
         placeholder="Search"
         className="w-full rounded-full border-2 border-gray-300 p-2 pl-10"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
+      {search.length > 0 && (
+        <XMarkIcon
+          className="absolute right-8 top-5 h-5 w-5 text-gray-800"
+          onClick={() => setSearch("")}
+        />
+      )}
       <Suggestions items={items} />
     </div>
   );
